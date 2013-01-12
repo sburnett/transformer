225ee1ca6ec6f366a2d9198886ef8bd7d7038749
Sam Burnett <srburnet@gmail.com>
2013-01-12 03:37:06 +0000

    Documentation

--- key/key.go
+++ key/key.go
@@ -1,3 +1,25 @@
+// This package encodes tuples of primitives (i.e., strings and integers) into
+// byte strings, preserving the order of the encoded tuples when they are are
+// sorted lexicographically. If a tuple (x1, x2, ..., xn) < (y1, y2, ..., yn),
+// then bytes.Compare(Encode(x1, x2, ..., xn), Encode<y1, y2, ..., yn) < 0.
+//
+// We must take special care when encoding primitives since their default binary
+// encodings aren't always lexicographic.
+//
+// We encode byte arrays with terminating null characters, so bytes arrays may
+// not contain embedded nulls.
+//
+// We encode unsigned integers in big endian order to preserve their order when
+// sorted lexicographically; little endian order doesn't have this property.
+// The two-complemenet representation for signed integers doesn't sort properly,
+// so we convert signed integers into unsigned integers by subtracting MinInt32
+// (i.e., adding the absolute value of MinInt32).
+//
+// TODO(sburnett): Add support for floating point numbers. The default
+// representation doesn't sort lexicographically for negative numbers or numbers
+// with negative exponents, so we would need to deconstruct and alter the
+// floating point representation.
+
 package key
 
 import (
@@ -94,6 +116,8 @@ func Read(reader *bytes.Buffer, toRead ...interface{}) error {
 	return nil
 }
 
+// Decode the given byte array into a series of primitives. The variadic
+// arguments must be pointers to primitives so that we may modify their values.
 func Decode(key []byte, toRead ...interface{}) error {
 	return Read(bytes.NewBuffer(key), toRead...)
 }
@@ -119,9 +143,9 @@ func Write(writer io.Writer, toWrite ...interface{}) error {
 		case int8:
 			var uint8value uint8
 			if value >= 0 {
-				uint8value = uint8(value)+(math.MaxUint8-math.MaxInt8)
+				uint8value = uint8(value) + (math.MaxUint8 - math.MaxInt8)
 			} else {
-				uint8value = uint8(value-math.MinInt8)
+				uint8value = uint8(value - math.MinInt8)
 			}
 			if err := Write(writer, uint8value); err != nil {
 				return err
@@ -133,9 +157,9 @@ func Write(writer io.Writer, toWrite ...interface{}) error {
 		case int16:
 			var uint16value uint16
 			if value >= 0 {
-				uint16value = uint16(value)+(math.MaxUint16-math.MaxInt16)
+				uint16value = uint16(value) + (math.MaxUint16 - math.MaxInt16)
 			} else {
-				uint16value = uint16(value-math.MinInt16)
+				uint16value = uint16(value - math.MinInt16)
 			}
 			if err := Write(writer, uint16value); err != nil {
 				return err
@@ -147,9 +171,9 @@ func Write(writer io.Writer, toWrite ...interface{}) error {
 		case int32:
 			var uint32value uint32
 			if value >= 0 {
-				uint32value = uint32(value)+(math.MaxUint32-math.MaxInt32)
+				uint32value = uint32(value) + (math.MaxUint32 - math.MaxInt32)
 			} else {
-				uint32value = uint32(value-math.MinInt32)
+				uint32value = uint32(value - math.MinInt32)
 			}
 			if err := Write(writer, uint32value); err != nil {
 				return err
@@ -161,9 +185,9 @@ func Write(writer io.Writer, toWrite ...interface{}) error {
 		case int64:
 			var uint64value uint64
 			if value >= 0 {
-				uint64value = uint64(value)+(math.MaxUint64-math.MaxInt64)
+				uint64value = uint64(value) + (math.MaxUint64 - math.MaxInt64)
 			} else {
-				uint64value = uint64(value-math.MinInt64)
+				uint64value = uint64(value - math.MinInt64)
 			}
 			if err := Write(writer, uint64value); err != nil {
 				return err
@@ -175,6 +199,7 @@ func Write(writer io.Writer, toWrite ...interface{}) error {
 	return nil
 }
 
+// Encode the parameters and return a byte array.
 func Encode(toWrite ...interface{}) ([]byte, error) {
 	buffer := bytes.NewBuffer([]byte{})
 	if err := Write(buffer, toWrite...); err != nil {
