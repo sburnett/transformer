36546814d71ed6a0e2c7c2ecb931b9c0058d2e92
Sam Burnett <srburnet@gmail.com>
2013-01-13 00:17:25 +0000

    Add parallel Do

--- transformers.go
+++ transformers.go
@@ -6,6 +6,33 @@ func (transformer TransformerFunc) Do(inputChan, outputChan chan *LevelDbRecord)
 	transformer(inputChan, outputChan)
 }
 
+type Doer interface {
+	Do(inputRecord *LevelDbRecord, outputChan chan *LevelDbRecord)
+}
+
+type DoFunc func(inputRecord *LevelDbRecord, outputChan chan *LevelDbRecord)
+
+func (doFunc DoFunc) Do(inputRecord *LevelDbRecord, outputChan chan *LevelDbRecord) {
+	doFunc(inputRecord, outputChan)
+}
+
+func MakeDoTransformer(doer Doer, numConcurrent int) Transformer {
+	return TransformerFunc(func(inputChan, outputChan chan *LevelDbRecord) {
+		doneChan := make(chan bool)
+		for i := 0; i < numConcurrent; i++ {
+			go func() {
+				for record := range inputChan {
+					doer.Do(record, outputChan)
+				}
+				doneChan <- true
+			}()
+		}
+		for i := 0; i < numConcurrent; i++ {
+			<-doneChan
+		}
+	})
+}
+
 type Mapper interface {
 	Map(inputRecord *LevelDbRecord) (outputRecord *LevelDbRecord)
 }
